59187b1fcd29c6d14ea11cf18a6f1ebd82f86e4b
Lubomir Marcinko <lubomir.marcinko@gmail.com>
2023-11-17 14:41:17 +0100

    deleted someting

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 
-import { CustomerField } from '@/app/lib/definitions';
-import Link from 'next/link';
+import { CustomerField } from "@/app/lib/definitions";
+import Link from "next/link";
 import {
   CheckIcon,
   ClockIcon,
   CurrencyDollarIcon,
   UserCircleIcon,
-} from '@heroicons/react/24/outline';
-import { Button } from '@/app/ui/button';
-import { createInvoice } from '@/app/lib/actions';
-import { useFormState } from 'react-dom';
+} from "@heroicons/react/24/outline";
+import { Button } from "@/app/ui/button";
+import { createInvoice } from "@/app/lib/actions";
+import { useFormState } from "react-dom";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState = { message: null, errors: {} };
@@ -43,16 +43,16 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
           {state.errors?.customerId ? (
-        <div
-          id="customer-error"
-          aria-live="polite"
-          className="mt-2 text-sm text-red-500"
-        >
-          {state.errors.customerId.map((error: string) => (
-            <p key={error}>{error}</p>
-          ))}
-        </div>
-      ) : null}
+            <div
+              id="customer-error"
+              aria-live="polite"
+              className="mt-2 text-sm text-red-500"
+            >
+              {state.errors.customerId.map((error: string) => (
+                <p key={error}>{error}</p>
+              ))}
+            </div>
+          ) : null}
         </div>
 
         {/* Invoice Amount */}
@@ -69,21 +69,20 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-        
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
-            {state.errors?.amount? (
-        <div
-          id="customer-error"
-          aria-live="polite"
-          className="mt-2 text-sm text-red-500"
-        >
-          {state.errors.amount.map((error: string) => (
-            <p key={error}>{error}</p>
-          ))}
-        </div>
-      ) : null}
+            {state.errors?.amount ? (
+              <div
+                id="customer-error"
+                aria-live="polite"
+                className="mt-2 text-sm text-red-500"
+              >
+                {state.errors.amount.map((error: string) => (
+                  <p key={error}>{error}</p>
+                ))}
+              </div>
+            ) : null}
           </div>
         </div>
         {/* Invoice Status */}
@@ -125,28 +124,17 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               </div>
             </div>
           </div>
-          {state.errors?.status? (
-        <div
-          id="customer-error"
-          aria-live="polite"
-          className="mt-2 text-sm text-red-500"
-        >
-          {state.errors.status.map((error: string) => (
-            <p key={error}>{error}</p>
-          ))}
-        </div>
-      ) : null}
-        {state.message? (
-        <div
-          id="customer-error"
-          aria-live="polite"
-          className="mt-2 text-sm text-red-500"
-        >
-          {state.message?.map ((error: string) => (
-            <p key={error}>{error}</p>
-          ))}
-        </div>
-      ) : null}
+          {state.errors?.status ? (
+            <div
+              id="customer-error"
+              aria-live="polite"
+              className="mt-2 text-sm text-red-500"
+            >
+              {state.errors.status.map((error: string) => (
+                <p key={error}>{error}</p>
+              ))}
+            </div>
+          ) : null}
         </fieldset>
       </div>
       <div className="mt-6 flex justify-end gap-4">
